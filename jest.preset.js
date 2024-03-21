886151c2253121d9e13a3c64f075a69dcf8f664c
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2024-03-21 16:31:06 -0300

    test(jest): add base configuration

--- jest.preset.js
+++ jest.preset.js
@@ -3,4 +3,7 @@ const nxPreset = require('@nx/jest/preset').default;
 module.exports = {
   ...nxPreset,
   prettierPath: null, // HACK: Prettier v3 is incompatible with Jest's inline snapshots
+  clearMocks: true,
+  collectCoverageFrom: ['**/*.(use-case|adapter|service).ts'],
+  setupFilesAfterEnv: ['jest-extended/all'],
 };
